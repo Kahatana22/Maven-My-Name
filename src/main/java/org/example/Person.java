78b6c5cf1994b96a75c0dfcb3a1978ca0a7cbacf
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-06-11 11:31:01 +0300

    Update Person.java

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,2 +1,11 @@
-package org.example;public class Person {
+package org.example;
+
+public class Person {
+    private String name;
+    private String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
 }
